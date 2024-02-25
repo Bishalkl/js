6bd9e32e6ca45c4d4f20a0bcf1020f4dbb63146b
Bishal koirala <bishal123koirala@gmail.com>
2024-02-25 18:50:06 +0545

    done

--- script.js
+++ script.js
@@ -40,30 +40,117 @@
 
 // Number Guessing Game
 
-const minNum = 1;
-const maxNum = 2;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+// const minNum = 1;
+// const maxNum = 2;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 
-// reset
-let attempt = 0;
-let guess;
-let running = true;
+// // reset
+// let attempt = 0;
+// let guess;
+// let running = true;
 
-while(running){
-    let guess = window.prompt(`Enter the number between ${minNum} - ${maxNum}.`);
-    if( guess == answer){
-        attempt ++;
-        console.log(`You have guessed it correctly in ${attempt}`);
-        running = false;
-    }else{
-        attempt++;
-    }
-}
+// while(running){
+//     let guess = window.prompt(`Enter the number between ${minNum} - ${maxNum}.`);
+//     if( guess == answer){
+//         attempt ++;
+//         console.log(`You have guessed it correctly in ${attempt}`);
+//         running = false;
+//     }else{
+//         attempt++;
+//     }
+// }
 
 
 // variable scope = where a variable is recognized 
 
 // my project is to make a program which helps to conver the Temperature conversion program
+// my project is to make dice roller
+// my project is to make random number game
+// my project is to generat a random password
+
+
+// spread opeartor : ... alows an iterable such as an array or string to be expanded into sperate elements (unpack the elements)
+
+
+
+ let number = [1,2,3,4,5];
+ let maximum = Math.max(...number);
+
+ let fruits = ["apple", "orange", "banana"];
+ let newfruits = [...fruits];
+ console.log(newfruits, maximum);
+
+
+// rest parameters = (...rest )
+
+// function openFridge(...foods){
+//     console.log(foods);
+// }
+
+
+// function getFood(...foods){
+//     return foods;
+// }
+
+// const food1 = "pizza";
+// const food2 = "hamburger";
+
+
+
+
+// openFridge(food1,food2);
+
+// const foods = getFood(food1, food2);
+// console.log(foods);
+
+
+// funciton sum
+function SumNumbers(...nums){
+    let result = 0;
+    for(let number of nums){
+        result += number;
+    }
+
+    return result / nums.length;
+}
+
+const average = SumNumbers(75,78,100,36);
+console.log(`The average is ${average}`);
+
+
+
+// callback
+hello(bye); 
+
+function hello(callback){
+    console.log("Hello");
+    callback();
+}
+
+function bye(){
+    console.log("bye");
+}
+
+
+
+
+// foreach
+let numberr = [54, 383, 68, 39, 34];
+
+numberr.forEach(function(numbers) {
+    if (checkNumbers(numbers)) {
+        console.log(numbers);
+    }
+});
+
+function checkNumbers(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 
 
@@ -74,8 +161,6 @@ while(running){
 // while loop and do while loop
 // for loop
 // variable scope  (local and global)!
-
-
 // arrays
 // push
 // pop
@@ -86,7 +171,23 @@ while(running){
 // fruit of fruits (short cut for loop  in arrays)
 // sort
 // reverse
- 
+// spread operator
+// rest parameter
+// join() 
+// callback a funnction that is passed as an argument to another function.
+// forEach()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
