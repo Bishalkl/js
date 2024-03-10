9848e702cf0225a9edb01c5a256a541b2a1eff6f
Bishal koirala <bishal123koirala@gmail.com>
2024-03-10 21:11:36 +0545

    done

--- TImer_project/script.js
+++ TImer_project/script.js
@@ -1,96 +1,74 @@
-// declare it 
+class Timer {
+    constructor(displayElement) {
+        this.displayElement = displayElement;
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0;
+        this.millisecond = 0;
+        this.intervalId = null;
+        this.isRunning = false;
+    }
+
+    start() {
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.intervalId = setInterval(() => {
+                this.updateTimer();
+                this.updateDisplay();
+            },15);
+        }
+    }
+
+    stop() {
+        if (this.isRunning) {
+            this.isRunning = false;
+            clearInterval(this.intervalId);
+        }
+    }
+
+    reset() {
+        this.stop();
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0;
+        this.millisecond = 0;
+        this.updateDisplay();
+    }
+
+    updateTimer() {
+        this.millisecond++;
+        if (this.millisecond >= 100) {
+            this.millisecond = 0;
+            this.second++;
+            if (this.second >= 60) {
+                this.second = 0;
+                this.minute++;
+                if (this.minute >= 60) {
+                    this.minute = 0;
+                    this.hour++;
+                    if (this.hour >= 24) {
+                        this.hour = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    updateDisplay() {
+        let paddedHour = this.hour.toString().padStart(2, '0');
+        let paddedMinute = this.minute.toString().padStart(2, '0');
+        let paddedSecond = this.second.toString().padStart(2, '0');
+        let paddedMillisecond = this.millisecond.toString().padStart(2, '0');
+        this.displayElement.textContent = `${paddedHour}:${paddedMinute}:${paddedSecond}:${paddedMillisecond}`;
+    }
+}
+
 const display = document.getElementById('display');
 const startbtn = document.getElementById('start');
 const stopbtn = document.getElementById('stop');
 const resetbtn = document.getElementById('reset');
 
-// intial value 
-let hourvalue = 0;
-let minvalue = 0;
-let secondvalue = 0;
-let milsecvalue = 0
-let isrunning = false;
-let intervalId = null;
-
-// function to chage the time value variable 
-function updateTImerValues(){
-    switch(true){
-        case milsecvalue >= 500:
-            milsecvalue = 0;
-            secondvalue++;
-            break;
-        case secondvalue >= 60:
-            secondvalue = 0;
-            minvalue++;
-            break;
-        case minvalue >=60:
-            minvalue = 0;
-            hourvalue++;
-            break;
-        case hourvalue >=24:
-            hourvalue++;
-            break;     
-    }
-}
-
-   
-// function to update time
-function update(){
-
-    // condition to get pad 0 start
-   let paddedhour = hourvalue.toString().padStart(2, 0);
-   let paddedmin = minvalue.toString().padStart(2, '0');
-    let paddedsec = secondvalue.toString().padStart(2, '0');
-    let paddedmilsec = milsecvalue.toString().padStart(3, '0');
-
-    // to display it html
-    display.textContent = `${paddedhour}:${paddedmin}:${paddedsec}:${paddedmilsec}`;
-} 
-
-
-
-// function for startTImer
-startbtn.onclick = function(){
-    if(isrunning === false){
-        // set it to running true 
-        isrunning = true;
-
-        // set intervalID
-        intervalId = setInterval(() => {
-
-            //milsec increment
-            milsecvalue++;
-            // udate a display
-            update();
-            // update time change value
-            updateTImerValues();
-        },1);
-        update();
-    }
-}
-
-// function for pauseTimer
-stopbtn.onclick = function(){
-    if(isrunning === true){
-        // set it to running false
-        isrunning = false;
-        // stop the timer
-        clearInterval(intervalId);
-    }
-}
-
-// function for resetTimer
-resetbtn.onclick = function(){
-        // set it to running false
-        isrunning = false;
-        // intervalid function
-        clearInterval(intervalId);
-        // setting into intial
-        hourvalue = 0;
-        minvalue = 0;
-        secondvalue = 0;
-        milsecvalue = 0;
-        update();
-}
-
-
+const timer = new Timer(display);
+startbtn.onclick = timer.start.bind(timer);
+stopbtn.onclick = timer.stop.bind(timer);
+resetbtn.onclick = timer.reset.bind(timer);
