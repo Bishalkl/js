e923407a4d8ab6a834336a56d49ea5e02c547456
Bishal koirala <bishal123koirala@gmail.com>
2024-03-11 14:20:37 +0545

    done

--- Random_generator_game/script.js
+++ Random_generator_game/script.js
@@ -1,26 +1,53 @@
-const display = document.getElementById("display");
-const valueInput = document.getElementById("input");
+// const display = document.getElementById("display");
+// const valueInput = document.getElementById("input");
 const btn = document.getElementById("btn");
 
-let min = 1;
-let max = 10;
+// it include whole mechanism
+class randomGenerator{
+
+    // constructor
+    constructor(update, input){
+        this.update= document.getElementById(update);
+        this.input = document.getElementById(input);
+        this.min = 1;
+        this.max = 10;
+        this.count = 0;
+    }
+
+    //generate method
+    randomNumber(){
+        return Math.floor(Math.random() * (this.max - this.min)) + this.min;   
+    }
 
 
-function randomGenerator(){
-    return  Math.floor(Math.random() * (max - min) + 1) + min;
+    // onclickstate
+    buttonClick(){
+        // declare it to use for condition
+        let number = this.randomNumber();
+        let inputValue = parseInt(this.input.value)
+        
+        // condition first for get input
+        if(!isNaN(inputValue) && Number.isInteger(inputValue)){
+            // condition second for get value 
+            if(inputValue === number){
+                this.count++;
+                this.update.textContent = `Congratulations! You have guessed it  ${this.count} in count.`;
+            } else if (inputValue > number) {
+                this.update.textContent = "Sorry, Try again, it is bigger";
+            } else {
+                this.update.textContent = "Sorry, Try again, it is smaller";
+            }
+
+       } else{
+            this.update.textContent = "Please enter a valid integer value";
+       }
+        
+    }
+
 }
 
-
+// testing
+const generateNew = new randomGenerator("display", "input");
 btn.onclick = function(){
-    let getValue = parseInt(valueInput.value);
-    let result = randomGenerator();
-    if(getValue === result){
-        display.textContent = `You have correct, Congratulation!`;
-    } else if( getValue < result){
-        display.textContent = `It is small, Try again`;
-    } else if( getValue > result){
-        display.textContent = `It is big, Try again`;
-    } else{
-        display.textContent = `Please enter the valid number`;
-    }
+    generateNew.buttonClick();
 }
